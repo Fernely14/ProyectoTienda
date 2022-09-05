6079b195ae99499960e989c02b373d0a3501cfa4
Fernely14 <fernely_14@hotmail.com>
2022-09-05 07:47:50 -0500

    actualizacion

--- Fabricantes.php
+++ Fabricantes.php
@@ -1,7 +1,7 @@
-   <!doctype html>
-   <html lang="en">
+<!doctype html>
+    <html lang="en">
   
-   <head>
+     <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,7 +9,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Fabricante</title>
+    <title>Fabricantes</title>
   
     </head>
     <body>
@@ -17,13 +17,13 @@
   <div class= "container">
   
   <br>
-  <form action="action/insertfabricante.php" method="$_POST">
+  <form action="action/insertfabricante.php" method="POST">
   <div class="mb-3">
     <label class="form-label">Nombre Fabricante</label>
     <input type="text" name= "inputNombre" class="form-control">
   
   </div>
-  <button type="submit" class="btn btn-primary">Enviar</button>
+  <button type="submit" name="enviar" class="btn btn-primary">Enviar</button>
 </form>
    
 
@@ -54,5 +54,4 @@
       <?php } ?>
 
       </tbody>
-    </table>
-   
+      </table>
